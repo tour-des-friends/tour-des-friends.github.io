a0d967e2fb320abba7393ea0334bef5031a971dc
speluxx <65849786+speluxx@users.noreply.github.com>
2023-09-13 20:08:06 +0200

    Datum geändert

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 22, 2024 00:00:00").getTime();
+var countDownDate = new Date("July 27, 2024 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
